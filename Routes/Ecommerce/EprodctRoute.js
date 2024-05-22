b3754f2db0893b4ce25fc447e3911e0f9d0c877e
Shubham.Lall <Shubham.lal@nw18.com>
2024-05-22 23:20:26 +0530

    added

--- Routes/Ecommerce/EprodctRoute.js
+++ Routes/Ecommerce/EprodctRoute.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import { addEproduct, getEProducts } from '../../Controller/Ecommerce/Eproduct.controller.js';
-import upload from '../../config/uploadImg';
+import upload from '../../config/uploadImg.js';
 
 router.post('/', upload.single('productImage'), addEproduct);
 router.get('/',getEProducts);
